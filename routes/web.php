880ee18d0139ceee0eec17234de31d9128ef029b
SKSteeve <steeve1997.ss@gmail.com>
2022-12-15 23:54:03 +0200

    Added general route for starting the app

--- routes/web.php
+++ routes/web.php
@@ -13,12 +13,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
-
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth'])->name('dashboard');
+Route::get('/{any}', [\App\Http\Controllers\AppController::class, 'index'])->where('any', '.*');
 
 require __DIR__.'/auth.php';
